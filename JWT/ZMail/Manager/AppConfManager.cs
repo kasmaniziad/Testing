1b5293bbce3ca0245879340ba1fdd94cb3b74e7e
kasmaniziad <kasmaniziad@outlook.com>
2016-12-28 13:11:12 +0300

    ZMail

---
+++ JWT/ZMail/Manager/AppConfManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Configuration;
+
+namespace ZMail.Managers
+{
+    public class AppConfManager
+    {
+        public static string FromEmailId { get { return Properties.Settings.Default.FromEmailId; } }
+
+        public static string MailServer { get { return Properties.Settings.Default.MailServer; } }
+
+        public static string ActivateUserMail { get { return Properties.Settings.Default.ActivateUserMail; } }
+
+        public static string UnableToSendEmail { get { return Properties.Settings.Default.UnableToSendEmail; } }
+
+    }
+}
